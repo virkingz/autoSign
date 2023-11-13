9bf8df7fb16bc58959890041e66115163a43ccef
virkingz <68280845+virkingz@users.noreply.github.com>
2023-11-13 09:52:44 +0800

    Update zjsc.py

--- zjsj/zjsc.py
+++ zjsj/zjsc.py
@@ -15,7 +15,7 @@ def qiandao(token):
     headers={
 	'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Mobile Safari/537.36'
     }
-    headers[Authorization]=token
+    headers['Authorization']=token
     data = get(url, headers=headers).json()
     print(data);
 
