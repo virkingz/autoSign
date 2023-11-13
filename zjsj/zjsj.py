a190b7f7a8724aba6822c582a34a71727cdf1781
virkingz <68280845+virkingz@users.noreply.github.com>
2023-11-13 09:39:15 +0800

    Create zjsj.py

---
+++ zjsj/zjsj.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -- coding: utf-8 --
+import os
+from datetime import date
+from json import dumps
+from requests import get, post
+# -- 获取token --
+os.getenv('ZJSJ_TOKEN')
+os.environ.get('ZJSJ_TOKEN')
+g_token=os.environ['ZJSJ_TOKEN']
+
+def qiandao(token):
+    tokens=token.split("@")
+    param={'site':'api','r':'checkin/checkin','tenant_code':'zjsjadmin','orgcode':'zjsjadmin'}
+    param['token']=tokens[0]
+    param['member_id']=tokens[1]
+    url='https://vip-php-platform.myscrm.cn/index.php'
+    data = post(url, params=param).json()
+    print(data);
+
+if __name__ == "__main__":
+    if token == "":
+        exit(0)
+    tokens = g_token.split("&")
+    for s in tokens:
+        qiandao(s)
