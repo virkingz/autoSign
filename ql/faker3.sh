3e12097a98c4266c871f34002050373e3d2b3721
virkingz <68280845+virkingz@users.noreply.github.com>
2024-12-10 10:26:02 +0800

    Delete ql directory

--- ql/faker3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#cron "10 0-23/5 * * *" script-path=faker3.sh,tag=faker3仓库更新
-if [ ! -d "/ql/data/scripts/faker3" ]; then
-    git clone  https://git.metauniverse-cn.com/https://github.com/shufflewzc/faker3.git /ql/data/scripts/faker3
-else
-    git -C /ql/data/scripts/faker3 reset --hard
-    git -C /ql/data/scripts/faker3 pull --rebase
-fi
